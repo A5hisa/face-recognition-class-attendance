faae46f264a2458ff6ace7fb13500d8b57bae4cc
A5hisa <themarch2546@gmail.com>
2024-12-22 11:12:09 +0700

    Update

---
+++ VideoCaptureThread.py
@@ -0,0 +1,37 @@
+import threading
+import queue
+import time
+import cv2
+
+
+
+class VideoCaptureThread(threading.Thread):
+    def __init__(self, src=0, width=640, height=480, queue_size=2):
+        super().__init__()
+        self.capture = cv2.VideoCapture(src, cv2.CAP_DSHOW)
+        self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+        self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+        self.capture.set(cv2.CAP_PROP_FPS, 60)
+        self.queue = queue.Queue(maxsize=queue_size)
+        self.stopped = False
+
+    def run(self):
+        while not self.stopped:
+            if not self.queue.full():
+                ret, frame = self.capture.read()
+                if not ret:
+                    self.stop()
+                    break
+                self.queue.put(frame)
+            else:
+                time.sleep(0.015)  # Prevent busy waiting
+
+    def read(self):
+        return self.queue.get()
+
+    def more(self):
+        return not self.queue.empty()
+
+    def stop(self):
+        self.stopped = True
+        self.capture.release()
