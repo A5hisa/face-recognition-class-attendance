faa9ee15c5b31441403ea07c9f6be341b87ab9d3
A5hisa <themarch2546@gmail.com>
2024-12-26 02:16:50 +0700

    Update class_attendance.py

--- class_attendance.py
+++ class_attendance.py
@@ -1,20 +1,2 @@
 # 0 = False
 # 1 = True
-
-import time
-
-has_checked = False
-
-class Attendance:
-    
-    def __init__(self, name):
-        self.name = name
-        self.check()
-
-    def check(self):
-        if self.name == "Unknown":
-            pass   
-        elif self.name != "Unknown" and not has_checked:
-            print(self.name)
-            has_checked = True
-            
