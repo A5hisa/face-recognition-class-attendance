6e767eb58aeefb449d7c80af8a03f99f0f690eaa
A5hisa <themarch2546@gmail.com>
2024-12-28 03:07:19 +0700

    Update class_attendance.py

--- class_attendance.py
+++ class_attendance.py
@@ -21,10 +21,24 @@ for filename in os.listdir(xlspath):
     df = pd.read_excel(os.path.join(xlspath, filename), engine="xlrd", sheet_name="Sheet")
     student_data = df.iloc[:,1:4]
     df_student_list = student_data.values.tolist()
-    for number, student_id, student_name in df_student_list :
+    for number, student_id, student_name in df_student_list :  
+
+        # check sect1 and sect2
+        if number == 1:
+            check_sect += 1
+            if check_sect == 2:
+                # create dataframe sect1
+                df_create1 = pd.DataFrame(final_student_list, columns= column_header)
+                # clear list for sect2
+                final_student_list = []  
+
+        # if number_student_no is int then add number, student_id, student_name to list for create data frame            
         if type(number) is int:
             new_list = [number, student_id, student_name,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-            final_student_list.append(new_list)
-    df_create = pd.DataFrame(final_student_list, columns= column_header)
-    print(df_create)
+            final_student_list.append(new_list)       
+
+    # if have sect2 create dataframe sect2
+    if check_sect == 2:
+        df_create2 = pd.DataFrame(final_student_list, columns= column_header)
+    with pd.ExcelWriter
     # df_create.to_excel("output.xlsx", index=False)
