269c658ca08f7609ab7a6f8d37af19b1334b0d59
A5hisa <themarch2546@gmail.com>
2024-12-29 02:02:35 +0700

    Update class_attendance.py

--- class_attendance.py
+++ class_attendance.py
@@ -1,5 +1,6 @@
-# 0 = False
-# 1 = True
+# For check attendance
+# 0 = False / 1 = True 
+
 import pandas as pd
 import os
 
@@ -9,49 +10,56 @@ xls_path = "raw_data"
 # attendance_path for xlsx file to check class attendance
 attendance_path = "data_attendance"
 
+# check filename list
+checkfile = []
 
 # for cleansing data 
 column_header = ['เลขที่', 'รหัสประจำตัว', 'ชื่อ', 'Week1', 'Week2', 'Week3', 'Week4', 'Week5', 'Week6', 'Week7', 'Week8', 'Week9', 'Week10', 'Week11', 'Week12', 'Week13', 'Week14', 'Week15', 'Week16']
-final_student_list = []
-check_sect = 0
+student_list = []
+have2sect = False
 
 def cleansing_data():
 
-    global check_sect, final_student_list
+    global student_list, checkfile, have2sect
+
+    # collect filename in to checkfile
+    for file in os.listdir(attendance_path):
+        file, _ = os.path.splitext(file)
+        checkfile.append(file)
 
     for filename in os.listdir(xls_path):
-        df = pd.read_excel(os.path.join(xls_path, filename), engine="xlrd", sheet_name="Sheet")
-        student_data = df.iloc[:,1:4]
-        df_student_list = student_data.values.tolist()
-        for number, student_id, student_name in df_student_list :  
+        df = pd.read_excel(os.path.join(xls_path, filename), engine="xlrd")
+        get_subject = df.iloc[2,1]
+        subject_id = get_subject.split(' ')[2]
 
-            # check if have section2
-            if number == 1:
-                check_sect += 1
-                if check_sect == 2:
-                    # create dataframe sect1
-                    df_create1 = pd.DataFrame(final_student_list, columns= column_header)
-                    # clear list sect1 for sect2
-                    final_student_list = []  
+        # check data is already cleansing?
+        if subject_id not in checkfile: 
+            print(f"Cleansing data {filename} subject: {subject_id}")
+            # get student data from sheet
+            student_data = df.iloc[8:,1:4] # start row8 column B:D
+            df_student_list = student_data.values.tolist()
+            for number, student_id, student_name in df_student_list :  
+                if number == "เลขที่":
+                    df_section1 = pd.DataFrame(student_list, columns= column_header)
+                    have2sect = True
+                    student_list = []
+                # if number is int then add [number, student_id, student_name and 0] 0 for week1-16 in to list for create data frame            
+                if type(number) is int:
+                    student = [number, student_id, student_name,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+                    student_list.append(student)    
+            if not have2sect:
+                df_section1 = pd.DataFrame(student_list, columns= column_header)
 
-            # if number is int then add [number, student_id, student_name and 0] 0 for week1-16 in to list for create data frame            
-            if type(number) is int:
-                new_list = [number, student_id, student_name,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-                final_student_list.append(new_list)       
-
-        # set format to create excelfile
-        name, file_format = os.path.splitext(filename)
-        name = f"{name}.xlsx"
-
-        # create excelfile in attendance_path
-        with pd.ExcelWriter(os.path.join(attendance_path, name)) as writer:
-            df_create1.to_excel(writer, sheet_name="section1", index=False)
-
-            # have section2 create dataframe and sheet for section2
-            if check_sect == 2:
-                df_create2 = pd.DataFrame(final_student_list, columns= column_header)
-                df_create2.to_excel(writer, sheet_name="section2", index=False)
-    check_sect = 0
+            # create excelfile in attendance_path
+            namefile = f"{subject_id}.xlsx"
+            with pd.ExcelWriter(os.path.join(attendance_path, namefile)) as writer:
+                df_section1.to_excel(writer, sheet_name="section1", index=False)
+                if have2sect:
+                    df_section2 = pd.DataFrame(student_list, columns= column_header)
+                    df_section2.to_excel(writer, sheet_name="section2", index=False)
+                print(f"Create file in folder:{attendance_path} filename:{namefile}\n")
+        student_list = []
+        have2sect = False
 
 
 # for filename in os.listdir(xlspath):
